b628d76367aa2ab3fbeef0fbf72d9f6970517758
cfarn <58476975+cfarn@users.noreply.github.com>
2022-07-19 17:13:13 +0200

    1 - 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Character from './components/Character'
 
 class App extends React.Component {
   constructor () {
@@ -22,7 +23,10 @@ class App extends React.Component {
 
 	render() {
 		return(
-			<h1>Game of thrones</h1>
+			<>
+			  <h1>Game of thrones</h1>
+        <Character/>
+			</>
 		)
 	}
 }
