a473aa6d2ce70f514942ec10ab4288a6af7e6fa1
cfarn <cinniefarnaud@gmail.com>
2022-07-20 14:12:10 +0200

    bonus 1 - last point

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Character from './components/Character'
+import Favorite from './components/Favorite'
 
 class App extends React.Component {
   constructor () {
@@ -27,9 +28,9 @@ class App extends React.Component {
     console.log(this.state)
   }
 
+  // méthode pour les favoris
   handleFavoriteClick(character) {
-    // this.setState({favorites: character})
-    // création d'un clone du tableau 
+    // création d'un clone du tableau grâce à la destructuration 
     const clonedFavorites = [...this.state.favorites]
     clonedFavorites.push(character)
     // mise à jour du tableau cloné
@@ -43,8 +44,8 @@ class App extends React.Component {
     console.log(this.state)
 		return(
 			<div className='container'>
-			  <h1 className='text-center'>Game of thrones</h1>
-        <div className='row gap-3 justify-content-center'>
+			  <h1 className='text-center p-5'>Game of thrones</h1>
+        <div className='row gap-3 gy-3 justify-content-center'>
           {
             // ajout d'une clé index selon les recommandations
             this.state.characters.map((character, index) => {
@@ -57,6 +58,20 @@ class App extends React.Component {
                   favorite = {() => {
                     this.handleFavoriteClick(character)}
                   }
+                />
+              )
+            })
+          }
+        </div>
+        
+        <div className='row gap-3 gy-3 justify-content-center'>
+          <h2 className='text-center p-5'>Favoris</h2>
+          {
+            this.state.favorites.map((favorite, index) => {
+              return(
+                <Favorite
+                  key = {index}
+                  name = {favorite.fullName}
                 />
               )
             })
